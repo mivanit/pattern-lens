aabd9881a86328d822bcc46d012b930cd17e8488
mivanit <mivanits@umich.edu>
2025-03-03 00:03:31 -0700

    fix __all__ in __init__ for docs

--- pattern_lens/__init__.py
+++ pattern_lens/__init__.py
@@ -1,3 +1,15 @@
 """
 .. include:: ../README.md
 """
+
+__all__ = [
+	"activations",
+	"attn_figure_funcs",
+	"consts",
+	"figure_util",
+	"figures",
+	"indexes",
+	"load_activations",
+	"prompts",
+	"server",
+]
